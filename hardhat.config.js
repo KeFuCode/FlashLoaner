098aaee7a1ceceb1c9d52ce67383402cfd0a4f1f
KeFuCode <fukeee@163.com>
2022-04-19 21:57:26 +0800

    FlashLoaner.sol contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,7 +16,7 @@ module.exports = {
     apiKey: process.env.ETHERSCAN_API_KEY
   },
   solidity: {
-    version: "0.8.0",
+    version: "0.6.6",
     settings: {
       optimizer: {
         enabled: true,
